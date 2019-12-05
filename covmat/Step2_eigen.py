3a84706a8f2f253db613cef92c9f4619e0f5fd19
David Rubin <drubin@stsci.edu>
2019-12-05 09:38:48 -1000

    updates for RH19

--- covmat/Step2_eigen.py
+++ covmat/Step2_eigen.py
@@ -12,35 +12,38 @@ def save_img(dat, imname):
     fitsobj.writeto(imname)
     fitsobj.close()
 
-the_count = 0
 
-d_mBx1c_dsys = []
-nsne = 740
+for include_pecvel in [0, 1]:
+    the_count = 0
 
-for fl in glob.glob("C*fits"):
-    if (fl.count("stat") == 0) or fl == "C_stat_new.fits":
-        print(fl)
-        f = fits.open(fl)
-        dat = f[0].data
-        f.close()
-        
-        evals, evecs = linalg.eig(dat)
-        evecs = transpose(evecs)
-        
-        evals = real(evals)
-        evecs = real(evecs)
-        
-        inds = argsort(evals)[::-1]
-        
-        for ind in inds:
-            if evals[ind] > evals[inds[0]]*1e-6:
-                d_mBx1c_dsys.append([])
-                for i in range(nsne):
-                    d_mBx1c_dsys[-1].append((evecs[ind]*sqrt(evals[ind]))[i*3:(i+1)*3])
+    d_mBx1c_dsys = []
+    nsne = 740
 
-d_mBx1c_dsys = array(d_mBx1c_dsys)
-print(d_mBx1c_dsys.shape)
+    for fl in glob.glob("C*fits"):
+        if (fl.count("stat") == 0) or fl == "C_stat_new.fits":
+            if (fl.count("pecvel") == 0) or include_pecvel:
+                print("include_pecvel", include_pecvel, fl)
+                f = fits.open(fl)
+                dat = f[0].data
+                f.close()
 
-save_img(d_mBx1c_dsys, "d_mBx1c_dsys.fits")
+                evals, evecs = linalg.eig(dat)
+                evecs = transpose(evecs)
+
+                evals = real(evals)
+                evecs = real(evecs)
+
+                inds = argsort(evals)[::-1]
+
+                for ind in inds:
+                    if evals[ind] > evals[inds[0]]*1e-6:
+                        d_mBx1c_dsys.append([])
+                        for i in range(nsne):
+                            d_mBx1c_dsys[-1].append((evecs[ind]*sqrt(evals[ind]))[i*3:(i+1)*3])
+
+    d_mBx1c_dsys = array(d_mBx1c_dsys)
+    print(d_mBx1c_dsys.shape)
+
+    save_img(d_mBx1c_dsys, "d_mBx1c_dsys_pecvel=%i.fits" % include_pecvel)
 
 
